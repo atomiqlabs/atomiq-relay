24d18a97daf214f9a4578f6d148275eb91a6db4b
adambor <adamborteam@gmail.com>
2025-07-03 13:30:50 +0200

    Update to new base lib version, add citrea

---
+++ src/chains/evm/CitreaChainInitializer.ts
@@ -0,0 +1,62 @@
+import {ChainInitializer} from "../ChainInitializer";
+import {
+    numberParser,
+    objectParser,
+    stringParser,
+    enumParser
+} from "@atomiqlabs/server-base";
+import {RootTemplate} from "../RootTemplate";
+import {initializeCitrea, EVMFees, CitreaChainType, EVMSigner, EVMSpvVaultData} from "@atomiqlabs/chain-evm";
+import {JsonRpcProvider} from "ethers";
+import {getEVMSigner} from "./signer/BaseEVMSigner";
+import {EVMChainEvents} from "@atomiqlabs/chain-evm/dist/evm/events/EVMChainEvents";
+
+const template = {
+    ...RootTemplate,
+    RPC_URL: stringParser(),
+    MAX_LOGS_BLOCK_RANGE: numberParser(false, 1, undefined, true),
+    MAX_FEE_GWEI: numberParser(false, 0),
+    CHAIN: enumParser(["MAINNET", "TESTNET4"]),
+
+    MNEMONIC_FILE: stringParser(null, null, true),
+    PRIVKEY: stringParser(66, 66, true)
+} as const;
+
+export const CitreaChainInitializer: ChainInitializer<CitreaChainType, any, typeof template> = {
+    loadChain: (directory, configuration, bitcoinRpc, bitcoinNetwork) => {
+        const provider = new JsonRpcProvider(configuration.RPC_URL);
+
+        const {chainInterface, btcRelay, swapContract, spvVaultContract} = initializeCitrea({
+            rpcUrl: provider,
+            chainType: configuration.CHAIN,
+            maxLogsBlockRange: configuration.MAX_LOGS_BLOCK_RANGE,
+            fees: new EVMFees(provider, BigInt(configuration.MAX_FEE_GWEI))
+        }, bitcoinRpc, bitcoinNetwork);
+
+        console.log("Init provider: ", provider);
+        const evmSigner = getEVMSigner(configuration);
+
+        const chainEvents = new EVMChainEvents(
+            directory, chainInterface, swapContract, spvVaultContract
+        );
+
+        const signer = new EVMSigner(evmSigner, evmSigner.address);
+
+        return {
+            chainId: "CITREA",
+            signer,
+            swapContract,
+            chainEvents,
+            chain: chainInterface,
+            spvVaultContract,
+            spvVaultDataCtor: EVMSpvVaultData,
+            btcRelay,
+            nativeToken: chainInterface.getNativeCurrencyAddress(),
+            nativeTokenDecimals: 18,
+            commands: []
+        };
+    },
+    configuration: objectParser(template, (data) => {
+        if(data.MNEMONIC_FILE==null && data.PRIVKEY==null) throw new Error("Mnemonic file or explicit private key must be specified!");
+    }, true)
+};
