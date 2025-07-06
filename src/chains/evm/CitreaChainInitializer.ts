5c84419c1a05e5069b00805a0cb0ba23ab15dd40
adambor <adamborteam@gmail.com>
2025-07-06 13:43:15 +0200

    Update libs

--- src/chains/evm/CitreaChainInitializer.ts
+++ src/chains/evm/CitreaChainInitializer.ts
@@ -6,7 +6,13 @@ import {
     enumParser
 } from "@atomiqlabs/server-base";
 import {RootTemplate} from "../RootTemplate";
-import {initializeCitrea, EVMFees, CitreaChainType, EVMSigner, EVMSpvVaultData} from "@atomiqlabs/chain-evm";
+import {
+    initializeCitrea,
+    CitreaChainType,
+    EVMSigner,
+    EVMSpvVaultData,
+    CitreaFees
+} from "@atomiqlabs/chain-evm";
 import {JsonRpcProvider} from "ethers";
 import {getEVMSigner} from "./signer/BaseEVMSigner";
 import {EVMChainEvents} from "@atomiqlabs/chain-evm/dist/evm/events/EVMChainEvents";
@@ -31,7 +37,7 @@ export const CitreaChainInitializer: ChainInitializer<CitreaChainType, any, type
             rpcUrl: provider,
             chainType: configuration.CHAIN,
             maxLogsBlockRange: configuration.MAX_LOGS_BLOCK_RANGE,
-            fees: new EVMFees(
+            fees: new CitreaFees(
                 provider,
                 BigInt(Math.floor(configuration.MAX_FEE_GWEI * 1_000_000_000)),
                 configuration.FEE_TIP_GWEI==null ? undefined : BigInt(Math.floor(configuration.FEE_TIP_GWEI * 1_000_000_000))
